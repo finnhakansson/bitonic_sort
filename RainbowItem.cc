73f774642925e3adcc78c0af44e2288c1077b35a
Finn Hakansson <finn_hakansson@yahoo.com>
2014-06-08 23:40:47 -0400

    WIP

--- RainbowItem.cc
+++ RainbowItem.cc
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 #include "Item.hh"
 #include "RainbowItem.hh"
 
@@ -18,19 +19,36 @@ RainbowItem::RainbowItem(int num) {
 RainbowItem::~RainbowItem() {}
 
 unsigned char RainbowItem::r() const {
+    return 0xdd;
 }
 
 unsigned char RainbowItem::g() const {
+    return 0x11;
 }
 
 unsigned char RainbowItem::b() const {
+    return 0x44;
 }
 
 static void RainbowItem::readRainbowColors() {
-    // Open file.
-    // Read the first line containing number of colors.
-    // Allocate colors array.
-    // Read all the colors.
+    ifstream inputStream("rainbow+white.rgb", ifstream::in);
+    colors = new unsigned char[237][3];
+    int index = 0;
+    int lineNumber = 0;
+    string line;
+    while (getline(inputStream, line) && index < 237) {
+        lineNumber++;
+        index++;
+        if (lineNumber <= 3) {
+            index = 0;
+            continue;
+        }
+        istringstream iss(line);
+        iss >> colors[index][0];
+        iss >> colors[index][1];
+        iss >> colors[index][2];
+    }
+    inputStream.close();
 }
 
 
